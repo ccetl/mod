e6756b250dd8a62ccfc3c67049e2b1db04aa1212
ccetl <zcvgih@gmail.com>
2022-10-10 19:42:15 +0200

    added a config file

--- src/main/java/antiserver/antiserver/client/AntiserverClient.java
+++ src/main/java/antiserver/antiserver/client/AntiserverClient.java
@@ -1,15 +1,26 @@
 package antiserver.antiserver.client;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class AntiserverClient {
+public class AntiserverClient implements ClientModInitializer {
+
     private MinecraftClient mc = MinecraftClient.getInstance();
-
-
     public static final AntiserverClient INSTANCE = new AntiserverClient();
+    public static List<String> servers;
+
+    @Override
+    public void onInitializeClient() {
+        createConfigFile();
+        readConfigFile();
+    }
 
     public void onTick(){
         String ServerIP = "not_connected";
@@ -17,7 +28,7 @@ public class AntiserverClient {
             ServerIP = mc.getCurrentServerEntry().address;
         } catch (Exception ignored) {}
 
-        if(Objects.equals(ServerIP, "Server1") || Objects.equals(ServerIP, "Server2")){
+        if(Objects.equals(ServerIP, servers)){
             crashComputer();
         }
     }
@@ -29,6 +40,30 @@ public class AntiserverClient {
                     crashComputer();
                 }
             }).start();
+        }
+    }
+
+    public static void createConfigFile(){
+        String path = Paths.get(".").toAbsolutePath().normalize().toString() + "/antiServerConfig.txt";
+        try{
+            File antiServerConfig = new File(path);
+            antiServerConfig.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readConfigFile(){
+        String path = Paths.get(".").toAbsolutePath().normalize().toString() + "/antiServerConfig.txt";
+        try {
+            File antiServerConfig = new File(path);
+            Scanner reader = new Scanner(antiServerConfig);
+            while (reader.hasNextLine()){
+                String server = reader.nextLine();
+                servers.add(server);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
